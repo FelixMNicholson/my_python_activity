b8ff3c58267707c1cfc44b19c3ac24e37dbb8891
FelixMNicholson <122548358+FelixMNicholson@users.noreply.github.com>
2023-01-16 12:47:01 +0100

    Add files via upload

---
+++ my_first_code.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+# Remember Python does not use a comma to separate thousands
+# Create variables
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+# Add all the sales together 
+total_sales = a + b + c + d + e
+
+# View the output
+print(total_sales)
+
+
+#Average Sales for the Month
+
+average_sales = total_sales / 5 
+
+print (average_sales)
+
+#show the property price for Property C 
+
+C_Property_Price = c - 6500
+
+print (C_Property_Price)
+
+
+# In[ ]:
+
+
+
+
